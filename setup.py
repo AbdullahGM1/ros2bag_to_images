6670d7af74c3b7aa894dea9b954c36486dd28012
Abdullah <141170550+AbdullahGM1@users.noreply.github.com>
2024-11-04 13:20:56 +0300

    update

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
-package_name = 'ros2bag_to_image'
+package_name = 'ros2bag_to_images'
 
 setup(
     name=package_name,
     version='1.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='AbdullahGM',
     maintainer_email='agm.musalami@gmail.com',
-    description='A package to extract images from a ROS2 bag file.',
+    description='Extract Images from Ros2 Humble Bag',
     license='MIT',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'extract_images = bag_image_extractor.extract_images:main',
+            'bag_image_extractor = ros2bag_to_images.bag_image_extractor:main'
         ],
     },
 )
-
